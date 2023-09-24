e832bf5b9cc3cf484c4af4283d68c44e74276050
Carina Lansing <carina.lansing@pnnl.gov>
2023-09-24 16:57:34 -0700

    Changed the way we find the code executions to invoke via python instead of using jq so it is more robust and easier to modify.

--- code_build/find_modified_pipelines.sh
+++ code_build/find_modified_pipelines.sh
@@ -7,19 +7,14 @@
 #               This script needs to be called from the root of your 
 #               deployment's pipelines repository.
 #####################################################################
+PIPELINES_DIR=$1
+CURRENT_COMMIT_HASH=$2
+PREVIOUS_COMMIT_HASH=$3
 
 CHANGE_FILE=/tmp/changelist
 
-# Use AWS CLI to look up the previous commit hash
-    # AWS_PIPELINE_NAME (e.g., ingest-buoy-dev-build-pipline) is passed into the
-    # environment from the stack.
-    # Alternatively, we could parse it from the $CODEBUILD_INITIATOR param that is set
-    # by AWS.  (e.g., CODEBUILD_INITIATOR=codepipeline/ingest-buoy-dev-build-pipline)
-PREVIOUS_COMMIT_HASH=$(aws codepipeline list-pipeline-executions --pipeline-name $AWS_PIPELINE_NAME | jq -r '[.pipelineExecutionSummaries[] | select(.status == "Succeeded") | .sourceRevisions][0][] | select(.actionName == "pipelines-source-action") | .revisionId')
-
 # Do a git diff to get the list of files that were changed in the latest commit
-    # CODEBUILD_RESOLVED_SOURCE_VERSION is set by CodePipeline and points to the most recent commit hash
-CURRENT_COMMIT_HASH=$CODEBUILD_RESOLVED_SOURCE_VERSION
+cd $PIPELINES_DIR
 git diff --name-only $CURRENT_COMMIT_HASH $PREVIOUS_COMMIT_HASH > $CHANGE_FILE
 
 # Parse out only the pipelines that changed
